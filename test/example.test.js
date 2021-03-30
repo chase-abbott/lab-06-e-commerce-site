da9e17a04df619668d3ffc9135b3483d961ba488
Chase Abbott <75389857+chase-abbott@users.noreply.github.com>
2021-03-30 15:43:56 -0700

    Update example.test.js

--- test/example.test.js
+++ test/example.test.js
@@ -7,7 +7,7 @@ const test = QUnit.test;
 
 test('It should take in a pet object and return an li element', (expect) => {
 
-    const expected = `<li class="Dog" style="background: green;"><p>Dog</p><p class="name">Spot</p><img src="/assets/dog.png"><p>Fluffy</p><p>Mammal</p><p>$100.00</p><button>Add to Cart!</button></li>`
+    const expected = `<li class="Dog" style="background: green;"><p>Dog</p><p class="name">Spot</p><img src="/assets/dog.png"><p>Fluffy</p><p>Mammal</p><p>$100.00</p><button>Add to Cart!</button></li>`;
     const actual = createPet({
         species: 'Dog',
         name: 'Spot',

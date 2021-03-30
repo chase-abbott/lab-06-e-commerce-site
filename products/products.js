eb5c5b5b246296a1e1fd8941b793406cf14aab0a
chase-abbott <cabbott93@gmail.com>
2021-03-30 13:23:52 -0700

    create html skeleton

---
+++ products/products.js
@@ -0,0 +1,46 @@
+const dog = {
+    id: 'dog',
+    name: 'spot',
+    image: './assets.dog.png',
+    isFluffy: true,
+    category: 'mammal',
+    price: 100,
+}
+
+const lizard = {
+    id: 'lizard',
+    name: 'zippy',
+    image: './assets.lizard.png',
+    isFluffy: false,
+    category: 'reptile',
+    price: 45,
+}
+
+const cat = {
+    id: 'cat',
+    name: 'purrfessor peaches',
+    image: './assets/cat.png',
+    isFluffy: true,
+    category: 'mammal',
+    price: 90,
+}
+
+const rabbit = {
+    id: 'rabbit',
+    name: 'hop',
+    image: './assets/rabbit.png',
+    isFluffy: true,
+    category: 'mammal',
+    price: 75,
+}
+
+const turtle = {
+    id: 'turtle',
+    name: 'rocky',
+    image: './assets.turtle.png',
+    isFluffy: false,
+    category: 'reptile',
+    price: 60,
+}
+
+export let array = [dog, lizard, cat, rabbit, turtle]

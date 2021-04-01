798a2acbeb615daeed33061372798f9a9452b7a0
Chase Abbott <75389857+chase-abbott@users.noreply.github.com>
2021-03-31 21:42:05 -0700

    Revert "Revert "Dev""

---
+++ cart/cart-data.js
@@ -0,0 +1,18 @@
+export const cart = [
+    {
+        id: 'Dog',
+        quantity: 3
+    },
+    {
+        id: 'Lizard',
+        quantity: 1
+    },
+    {
+        id: 'Frog',
+        quantity: 6
+    },
+    {
+        id: 'Cat',
+        quantity: 2
+    },
+];
